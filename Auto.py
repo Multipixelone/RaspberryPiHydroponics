e4379d60105afc85b6a1b4f20c58ba70222233da
Multipixelone <5051116+Multipixelone@users.noreply.github.com>
2017-10-14 15:54:24 -0700

    Made the code execute

--- Auto.py
+++ Auto.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # Made by Multipixelone
 # Automatic Plant Control, With Defaults set from the Install script!
+from time import sleep
 import Pump
 import Lights
 import schedule
+print("Welcome to RaspberryPiHydroponics, By Multipixelone!")
+print("Starting schedule...")
 
 # Schedule
 schedule.every().day.at("10:30").do(Pump.Flood)
 schedule.every().day.at("7:00").do(Lights.LightsOn)
 schedule.every().day.at("13:00").do(Lights.LightsOff)
+
+while True:
+    schedule.run_pending()
+    sleep(1)
