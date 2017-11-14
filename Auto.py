dd72af9f34f779ec20295e24f595491dacc4e162
multipixelone <5051116+Multipixelone@users.noreply.github.com>
2017-11-14 11:11:17 -0800

    Add another flood time

--- Auto.py
+++ Auto.py
@@ -10,8 +10,9 @@ print("Starting schedule...")
 
 # Schedule
 schedule.every().day.at("10:30").do(Pump.Flood)
+schedule.every().day.at("13:00").do(Pump.Flood)
 schedule.every().day.at("7:00").do(Lights.LightsOn)
-schedule.every().day.at("13:00").do(Lights.LightsOff)
+schedule.every().day.at("13:30").do(Lights.LightsOff)
 
 while True:
     schedule.run_pending()
