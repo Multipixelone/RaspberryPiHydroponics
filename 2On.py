3d913f94db3d4ebfbe6c7b3b9a0db436e4956b54
Multipixelone <5051116+Multipixelone@users.noreply.github.com>
2017-11-24 14:58:24 -0800

    Removed old files

--- 2On.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# Made by Multipixelone
-# Controller for Lights
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.OUT)
-GPIO.output(2, 1)
-print("Lights On")
-GPIO.cleanup()
